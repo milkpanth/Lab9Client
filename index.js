b3f368a67104c1e77c79504232d15de0f9feea2e
milkpanth <milkpanth@yahoo.com>
2020-04-16 13:44:01 +0700

    3

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var app = express();
 app.get('/', (req, res) => res.send('Hello world') );
+
+app.get('/crash',(req,res)=>{
+    console.log('Crash!!')
+    res.send('Crash')
+    process.exit(1)
+})
+
 app.listen(80,()=>console.log('Server Running'));
